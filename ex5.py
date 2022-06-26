ac010ef653df2f8426a481f8c76e3cd7afed1e70
noyzini <102484852+noyzini@users.noreply.github.com>
2022-06-26 17:49:24 +0300

    first commend

--- ex5.py
+++ ex5.py
@@ -13,15 +13,29 @@ the course with the name "course_name".
 
 def names_of_registered_students(input_json_path, course_name):
     students_list = []
-    with open(input_json_path) as data_file:  #os.poen?
+    with open(input_json_path) as data_file:  # os.poen?
         data = json.load(data_file)
         for id_key in data:
-            if data[id_key]['registered_courses'][0] == course_name:
+            if course_name in data[id_key]['registered_courses']:
                 students_list.append(data[id_key]['student_name'])
     return students_list
 
 
 def enrollment_numbers(input_json_path, output_file_path):
+    courses_dict = {}
+    with open(input_json_path) as data_file:  # os.poen?
+        data = json.load(data_file)
+        for id_key in data:
+            for student_curses in data[id_key]['registered_courses']:
+                if student_curses not in courses_dict.keys():
+                    courses_dict[student_curses] = 1
+                else:
+                    courses_dict[student_curses] = courses_dict[student_curses] + 1
+    with open(output_file_path, 'w') as outfile:
+        for key in sorted(courses_dict):
+            outfile.write('"'+key+'" '+str(courses_dict[key])+'\n')
+
+
     """
     This function writes all the course names and the number of enrolled
     student in ascending order to the output file in the given path.
