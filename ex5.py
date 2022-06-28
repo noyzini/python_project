428f6dd91eba44de7953ca0b9961ef034d30eee8
AviaAvraham1 <avia.avraham@campus.technion.ac.il>
2022-06-28 15:53:05 +0300

    hw5

--- ex5.py
+++ ex5.py
@@ -1,28 +1,35 @@
 import json
 import os
 
-"""
-This function returns a list of the names of the students who registered for
-the course with the name "course_name".
-
-:param input_json_path: Path of the students database json file.
-:param course_name: The name of the course.
-:return: List of the names of the students.
-"""
 
 
 def names_of_registered_students(input_json_path, course_name):
+    """
+    This function returns a list of the names of the students who registered for
+    the course with the name "course_name".
+
+    :param input_json_path: Path of the students database json file.
+    :param course_name: The name of the course.
+    :return: List of the names of the students.
+    """
     students_list = []
     with open(input_json_path) as data_file:  #os.poen?
         data = json.load(data_file)
-        return [data[id_key]["student_name"] for id_key in data if course_name in data[id_key]["registered_courses"]]
-        #for id_key in data:
-            #if (course_name in data[id_key]['registered_courses']):
-                #students.append(data[id_key]["student_name"]);
+        #return [data[id_key]["student_name"] for id_key in data if course_name in data[id_key]["registered_courses"]]
+        for id_key in data:
+            if course_name in data[id_key]['registered_courses']:
+                students_list.append(data[id_key]["student_name"]);
     return students_list
 
 
 def enrollment_numbers(input_json_path, output_file_path):
+    """
+    This function writes all the course names and the number of enrolled
+    student in ascending order to the output file in the given path.
+
+    :param input_json_path: Path of the students database json file.
+    :param output_file_path: Path of the output text file.
+    """
     courses_dict = {}
     with open(input_json_path) as data_file:  # os.poen?
         data = json.load(data_file)
@@ -35,16 +42,6 @@ def enrollment_numbers(input_json_path, output_file_path):
     with open(output_file_path, 'w') as outfile:
         for key in sorted(courses_dict):
             outfile.write('"'+key+'" '+str(courses_dict[key])+'\n')
-
-
-    """
-    This function writes all the course names and the number of enrolled
-    student in ascending order to the output file in the given path.
-
-    :param input_json_path: Path of the students database json file.
-    :param output_file_path: Path of the output text file.
-    """
-    pass
 
 
 def courses_for_lecturers(json_directory_path, output_json_path):
@@ -68,14 +65,7 @@ def courses_for_lecturers(json_directory_path, output_json_path):
                             if course_name not in lecturers_dict[lecturer]:
                                 lecturers_dict[lecturer].append(course_name)
 
-    #print(lecturers_dict)
     with open(output_json_path, 'w') as writeTo:
         json.dump(lecturers_dict, writeTo, indent=4)
 
     return lecturers_dict
-
-
-students = []
-input_json_path = "c:\Compiles C AVIA\hw5\students_database.json";
-course_name = "Introduction to Algorithms";
-print(names_of_registered_students(input_json_path,course_name));
